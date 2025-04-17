4097f90a2162c33b47a75ef090ddc27fa573dae8
Ivan.Reimand <145450323+IvanReimand@users.noreply.github.com>
2025-04-17 15:10:54 +0300

    Update java.js

--- java.js
+++ java.js
@@ -1,1 +1,19 @@
+///Määrame suurus///
+const mazeWidth = 20;
+const mazeHeight = 20;
+const maze = [];
 
+for (let i=0; i<mazeHeight, i++) {
+    maze[i] = [];
+    for (let j=0; j<mazeWidth; j++) {
+        maze[i][j] = 1;
+    }
+}
+function generateMaze(x, y) {
+    const directions = [
+        [-1, 0], // üles
+        [1, 0],  // alla
+        [0, -1], // vasak
+        [0, 1],  // parem
+]};
+directions.sort(() => Math.random() - 0.5);
